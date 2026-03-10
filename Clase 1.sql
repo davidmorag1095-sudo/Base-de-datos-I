1b8a8bead765b9b5f7a17cb17cb3b70efa8c91d4
davidmorag1095-sudo <davidmorag1095@gmail.com>
2026-03-10 16:51:04 -0600

    clase 1

--- Clase 1.sql
+++ Clase 1.sql
@@ -3,12 +3,33 @@ USE Curso;
 SET IMPLICIT_TRANSACTIONS on;
 
 
+drop table estudiantes;
+
 --Crear tabla de estudiantes--
 CREATE TABLE Estudiantes ( 
 	id					INT				IDENTITY(1,1),
-	nombre				VARCHAR(100)	NOT NULL,
+	Nombre				VARCHAR(100)	NOT NULL,
 	apellidos			VARCHAR(100)	NOT NULL,
 	email				VARCHAR(100)	UNIQUE,
 	fecha_registro		DATE			DEFAULT GETDATE(),
 	PRIMARY KEY(id)
-);
+  );
+
+--Insertar datos de ejemplo
+  INSERT INTO Estudiantes (nombre, apellidos, email)
+  VALUES
+	 ('Ana', 'Garcia', 'ana@gmailcom'),
+	 ('Carlos', 'Martinez', 'carlos@gmail.com'),
+	 ('Lucia', 'Fernandez', 'lucia@gmail.com');
+
+--Ver todos los registros
+SELECT * FROM Estudiantes;
+
+--Contar Registros
+SELECT COUNT(*) AS Total
+FROM Estudiantes; 
+
+--Buscar por nombre
+SELECT nombre, apellidos
+FROM Estudiantes
+WHERE nombre LIKE 'A%';
