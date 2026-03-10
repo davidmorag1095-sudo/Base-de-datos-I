9fd8061e120fba3adcf22cbdd42af17ba756ceca
davidmorag1095-sudo <davidmorag1095@gmail.com>
2026-03-10 16:28:13 -0600

    Clase 1

--- Clase 1.sql
+++ Clase 1.sql
@@ -6,7 +6,7 @@ SET IMPLICIT_TRANSACTIONS on;
 --Crear tabla de estudiantes--
 CREATE TABLE Estudiantes ( 
 	id					INT				IDENTITY(1,1),
-	Nombre				VARCHAR(100)	NOT NULL,
+	nombre				VARCHAR(100)	NOT NULL,
 	apellidos			VARCHAR(100)	NOT NULL,
 	email				VARCHAR(100)	UNIQUE,
 	fecha_registro		DATE			DEFAULT GETDATE(),
